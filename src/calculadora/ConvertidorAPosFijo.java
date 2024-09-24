1b54b5e06b8d1a0c43313ad591c80a09a2928c2a
JuliaRojasPe <juliarojasmx@gmail.com>
2024-09-24 12:36:28 -0600

    Update ConvertidorAPosFijo.java

--- src/calculadora/ConvertidorAPosFijo.java
+++ src/calculadora/ConvertidorAPosFijo.java
@@ -127,6 +127,7 @@ public class ConvertidorAPostFijo {
             }
            i++;
         }
+        //revisar que al encontrar un ? multiplique el numero por -1 para negativos
         if(Character.isDigit(cadenaInFijo.charAt(i)) || (".%".indexOf(cadenaInFijo.charAt(i)) != -1))
             postfijo.append(cadenaInFijo.charAt(i) + "_");
 
